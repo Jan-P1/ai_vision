c62b4a220ebba895b6ec0bf838d1def286718502
Jan-P1 <jan.akisuna@gmail.com>
2025-05-17 01:02:59 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# Create a virtual environment for the project
+# python -m venv esp_vision_env
+# Enter virtual environment
+# source ./esp_vision_env/bin/Activate.ps1
+# Install required packages
+# pip install -r requirements.txt
